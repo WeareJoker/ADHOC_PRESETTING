b200438b8908dd832bf8a16955815a6a559e92ef
kcrong <kcrong@kim82536.pe.kr>
2016-11-04 10:25:00 +0900

    커밋로그 적을 시간이 없다

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, session
+from flask import redirect
+from flask import request
+from flask import url_for
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development-key'
+
+
+def get_interface_by_script():
+    return os.popen('bash shell_scripts/get_interface.sh').read().split()[0]
+
+
+@app.route('/refresh')
+def refresh():
+    raw_essid_data = os.popen('iwlist %s scan | grep ESSID' % get_interface_by_script()).read()
+    essid_list = [essid.strip()[7:-1] for essid in raw_essid_data.split('\n')]
+    session['essid_list'] = essid_list
+
+    return redirect(url_for('index'))
+
+
+@app.route('/get_passwd/<string:ssid>', methods=['GET', 'POST'])
+def get_password(ssid=None):
+    if ssid is None:
+        return redirect(url_for('index'))
+    else:
+        session['ssid'] = ssid
+
+    if request.method == 'GET':
+        return render_template('passwd.html')
 
 
 @app.route('/')
 def index():
+    if session.get('essid_list') is None:
+        return redirect(url_for('refresh'))
     return render_template('main.html')
 
 
--- shell_scripts/connect_wifi.sh
+++ shell_scripts/connect_wifi.sh
@@ -12,11 +12,11 @@ clear
 ## Loop through available interfaces.
 while read interface; do                    # While reads a line of the output
     i=$((i+1))                                  # Only God knows what does this (view note 1).
-    type=$(cut -f2 -d ' ' <<< $interface)       # Saves the interface type to check if is wifi.
-    status=$(cut -f3 -d ' ' <<< $interface)     # Saves the status of the current interface.
-    interface=$(cut -f1 -d ' ' <<< $interface)  # Selects the INTEFACE field of the output.
+    type=$(cut -f2 -d ' ' <<< ${interface})       # Saves the interface type to check if is wifi.
+    status=$(cut -f3 -d ' ' <<< ${interface})     # Saves the status of the current interface.
+    interface=$(cut -f1 -d ' ' <<< ${interface})  # Selects the INTERFACE field of the output.
     if [[ "$type" == "802-11-wireless" ]]; then # If is a WiFi interface then:
-      interfaces[$i]=$interface                     # Adds the current interface to an array.
+      interfaces[$i]=${interface}                     # Adds the current interface to an array.
       echo "$i: $interface ($status)"               # Prints the name of current interface.
     fi                                          # Ends the if conditional
 done < <(nmcli device | tail -n +2)         # Redirects the output of the command nmcli device to the loop.
@@ -31,7 +31,7 @@ else
 fi
 
 ## If the entered number is valid then...
-if [[ "$iface" -le $i ]]; then
+if [[ "$iface" -le ${i} ]]; then
     read -p "Enter the SSID or BSSID: " b_ssid # Prompts the user for the ESSID/BSSID
     read -p "Enter the password: " pass # Prompts the user for the password
     output=$(nmcli device wifi connect "$b_ssid" password "$pass" iface wlan0 --timeout 10) # Tries to connect
