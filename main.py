316019f185ba7a806a1f2510c141a030620db081
DMJ <jo4965@gmail.com>
2016-11-23 08:10:14 -0800

    + executing dot11decrypt

--- main.py
+++ main.py
@@ -10,15 +10,41 @@ app.config['SECRET_KEY'] = 'development-key'
 
 
 def get_interface_by_script():
-    return os.popen('bash shell_scripts/get_interface.sh').read().split()[0]
+    result = os.popen('bash shell_scripts/get_interface.sh').read().split()
+    if result:
+        return result[0]
+    else:
+        return None
 
 
 @app.route('/refresh')
 def refresh():
-    raw_essid_data = os.popen('iwlist %s scan | grep ESSID' % get_interface_by_script()).read()
+    #catch channel and essid from "iwlist" command.
+    #raw_essid_data = os.popen('iwlist %s scan | grep -E "ESSID|Channel:"' % get_interface_by_script()).read()
+    
+    raw_essid_data = os.popen('iwlist %s scan | grep ESSID' % "wlx18a6f71cbc0b").read()
     essid_list = [essid.strip()[7:-1] for essid in raw_essid_data.split('\n')]
-    session['essid_list'] = essid_list
+    
+    now_decrypting = os.popen('ps -e | grep dot11decrypt').read()
+    if not now_decrypting:
+        now_decrypting = 0
+    else:
+        now_decrypting = 1
 
+
+    #ap_info_hash = []
+    #for info in raw_essid_data.split('\n'):
+    #    # first, is it channel?
+    #    #is_channel = info.find("Channel")
+    #    #if is_channel != -1:   # takes much cost
+    #    if info.strip()[0] == "C" : # is it channel?
+    #       chan = info.strip()[8:]
+    #    elif info.strip()[0] == "E" # is it essid?
+    #        ess = info.strip()[7:-1]
+    #        ap_info_hash
+
+    session['essid_list'] = essid_list
+    session['now_decrypting'] = now_decrypting
     return redirect(url_for('index'))
 
 
@@ -41,12 +67,52 @@ def get_password(ssid=None):
                                    ssid=ssid,
                                    message="Fail...")
 
+@app.route('/get_passwd_sniff/<string:ssid>', methods=['GET', 'POST'])
+def get_password_sniff(ssid=None):
+    if ssid is None:
+        return redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('passwd_sniff.html',
+                               ssid=ssid)
 
+    elif request.method == 'POST':
+        password = request.form['password']
+        # get channel info
+        iface = "wlx18a6f71cbc0b"
+        raw_channel_data = os.popen('iwlist %s scan | grep -E "ESSID|Channel:"' % iface).read()
+        
+        for info in raw_channel_data.split('\n'):
+            # is it ESSID?
+            if info.strip()[0] == "E":
+                if info.strip()[7:-1] == ssid:
+                    channel = tmp_chan
+                    break
+
+            # is it channel?
+            else:
+                tmp_chan = info.strip()[8:]
+
+        with open("/pjhs/autorun_setting", "wt") as f:
+            f.write("%s %s %s %s" % ("wpa", ssid, password, channel))
+        os.popen('/pjhs/dot11decrypt/build/dot11decrypt %s %s:%s:%s &'%(iface,"wpa", ssid, password))
+        session['now_decrypting'] = 1
+    return redirect(url_for('index'))
+
+@app.route('/stop_sniff')
+def stop_sniff():
+    dot11_pid = os.popen("ps -ef | grep dot11decrypt | awk '{print $2}'").read()
+    print(dot11_pid)
+    if len(dot11_pid.split('\n')) < 2:
+        return redirect(url_for('index'))
+
+    os.popen("kill -9 %s"%dot11_pid)
+    session['now_decrypting'] = 0
+    return redirect(url_for('index'))
 @app.route('/')
 def index():
     if session.get('essid_list') is None:
         return redirect(url_for('refresh'))
-    return render_template('main.html')
+    return render_template('main.html', now_decrypting=session['now_decrypting'])
 
 
 if __name__ == '__main__':
